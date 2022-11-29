4b0a19abb23d54386cddf46d1782ee8586f71e69
GasparAM <gaspar1999@live.com>
2022-11-29 22:06:17 +0400

    updated screen configs

--- .config/screensetup.sh
+++ .config/screensetup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-I-0 --mode 1280x1024 --pos 0x0 --rotate normal --output DVI-I-1 --off --output HDMI-0 --primary --mode 1920x1080 --pos 1280x0 --rotate normal --output DP-0 --off --output DP-1 --off --output VGA-1-1 --off
+xrandr --output DVI-I-0 --mode 1280x1024 --pos 0x0 --rotate normal --output DVI-I-1 --off --output HDMI-0 --primary --mode 1920x1080 --pos 1280x0 --rotate normal --output DP-0 --off --output DP-1 --off
